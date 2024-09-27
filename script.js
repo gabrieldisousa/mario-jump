271f99bb081b0606323d01cd0004625be925dcd8
Gabriel Diego <gabrieldi_sousa@yahoo.com.br>
2024-09-27 15:48:01 -0300

    score, game tip

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 mario = document.querySelector ('.mario');
 pipe = document.querySelector ('.pipe');
 clouds = document.querySelector ('.clouds');
-atualize = document.querySelector ('.atualize')
+atualize = document.querySelector ('.atualize');
+score = 0;
+scoreDisplay = document.querySelector('.score'); // Elemento onde a pontuação será exibida
+scored = false; // Flag para controlar o incremento da pontuação
 
 jump = () => {
     mario.classList.add('jump');
@@ -14,11 +17,19 @@ jump = () => {
     }, 500)
 }
 
+pipe.style.visibility = 'hidden'; // Pipe fica invisível
+// Após 1 segundo, o pipe aparece e a animação começa
+setTimeout(() => {
+    pipe.style.visibility = 'visible'; // Pipe se torna visível
+}, 3000);
+
+
 loop = setInterval(() => {
 
     pipePosition = pipe.offsetLeft;
     marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
     cloudsPosition = clouds.offsetLeft;
+
 
     if (pipePosition <= 107 && pipePosition > 0 && marioPosition < 80) {
 
@@ -43,9 +54,18 @@ loop = setInterval(() => {
         mario.style.left = '45px'
       
         clearInterval(loop);
+    }   else if (pipePosition < 0 && pipePosition > -50) {
+        // Se o Mario passou pelo pipe com sucesso
+        score++; // Incrementa a pontuação
+        scoreDisplay.innerText = `Score: ${score}`; // Atualiza o display da pontuação
+        scored = true; // Marca que a pontuação foi incrementada
+    }
+         // Quando o pipe sai da tela completamente, reseta a flag
+        if (pipePosition < -50) {
+        scored = false; // Reseta para permitir o próximo incremento
     }
 
-}, 10)
+}, 50)
 
 document.addEventListener('keydown', jump);
 document.addEventListener('touchstart', jump);
